4fa8b960ce5b29abccf8e8a8583af055df5359ea
jack阳 <you@example.com>
2020-03-11 18:05:07 +0800

    first commit

---
+++ src/main/java/com/iflytek/voicedemo/ActivityCompat.java
@@ -0,0 +1,11 @@
+package com.iflytek.voicedemo;
+
+class ActivityCompat {
+    public static void requestPermissions(MainActivity mainActivity, String[] strings, int i) {
+    }
+
+    public static int checkSelfPermission(MainActivity mainActivity, String writeExternalStorage) {
+        return 1;
+    }
+
+}
